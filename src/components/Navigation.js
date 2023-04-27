9b21ab22547cfe5f3883ed4db2e75a6dfe13b678
Anthony <antharhythm@Anthonys-MacBook-Pro.local>
2023-04-27 09:26:50 -0400

    added search bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -26,7 +26,7 @@ const Navigation = ({ account, setAccount }) => {
 
       <div className="nav__brand">
         <img src={logo} alt="Logo" />
-        <h1>Millow</h1>
+        <h1>Willow</h1>
       </div>
 
       {account ? (
